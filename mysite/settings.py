4ee915f24f8580b939c5dba70b1893c282613e9c
AxelOlea <axelcedilloolea@gmai.com>
2022-12-05 08:54:45 -0600

    Update for deploy AWS

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,19 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-import os
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-%%r)&&m4zeb$jnvvc6us)%@r=8o34q#@3-te(#gx*tv(0&bcy&')
+SECRET_KEY = 'django-insecure-%%r)&&m4zeb$jnvvc6us)%@r=8o34q#@3-te(#gx*tv(0&bcy&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = []
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -79,21 +73,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-import dj_database_url
-
 DATABASES = {
-    """
     # SQLite3 Database
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-    """
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-        )
 }
 
 
